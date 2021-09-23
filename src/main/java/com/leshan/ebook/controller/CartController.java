a244b90aac4e2b7e82583fd9f330af15fd77e12a
Mr.Xiang <406862257@qq.com>
2021-09-23 11:29:27 +0800

    完成添加购物车功能

--- src/main/java/com/leshan/ebook/controller/CartController.java
+++ src/main/java/com/leshan/ebook/controller/CartController.java
@@ -1,21 +1,45 @@
 package com.leshan.ebook.controller;
 
 import com.leshan.ebook.mybatis.entity.Cart;
+import com.leshan.ebook.service.CartService;
 import com.leshan.ebook.utils.ResponseResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/cart")
 public class CartController {
 
+    @Resource
+    private CartService cartService;
+
     @RequestMapping("/add")
     @ResponseBody
-    public ResponseResult add(Cart cart){
+    public ResponseResult add(Cart cart, HttpSession session){
         //测试是否能正常得到数据
         System.out.println(cart);
 
-        return null;
+        //得到用户id
+        int userid = (Integer) session.getAttribute("userid");
+
+        //将用户id设置到cart对象中
+        cart.setUserid(userid);
+
+        boolean result = cartService.addCart(cart);
+
+        ResponseResult responseResult = new ResponseResult();
+        if (result){
+            responseResult.setCode(200);
+            responseResult.setMessage("添加购物车成功");
+        }else {
+            responseResult.setCode(500);
+            responseResult.setMessage("系统正忙，请稍后再试");
+        }
+
+        return responseResult;
     }
 }
