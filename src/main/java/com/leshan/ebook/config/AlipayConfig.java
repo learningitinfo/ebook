5a324a15f43b70546c471888dfb37b086f9314cf
Mr.Xiang <406862257@qq.com>
2021-09-29 16:55:33 +0800

    支付宝后台实现

---
+++ src/main/java/com/leshan/ebook/config/AlipayConfig.java
@@ -0,0 +1,60 @@
+package com.leshan.ebook.config;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class AlipayConfig {
+    //↓↓↓↓↓↓↓↓↓↓请在这里配置您的基本信息↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+
+    // 应用ID,您的APPID，收款账号既是您的APPID对应支付宝账号
+    public static String app_id = "2016100100639421";
+
+    // 商户私钥，您的PKCS8格式RSA2私钥
+    public static String merchant_private_key = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDBQ/bVvKP0NUeMRCcqko0rIKVAsAg3c8C0Fy4haptgOCNshS2P8AQEfVstTcbEhuGQvqOCi2rJ5xzPWeCHHgudoIugxERd16SSTQODklIjz3lUc6EGKLoAh2AzYEGST5Ib6UFlyVG6Trk8UFYRkDGgsV6/bNE6F8ewb81xrZ0Cw5V+FtbmMT+UzaUipZFgqopMD8FBOHZmpLFtaHLAjYisihgI2mP5WPnBoPPB4LjTw2SYKxlnPpeuwBPUZCfGyi2ViE4OitKUnsTvxJ0oEfJD+2PGDK8ZKwcsESikTid5YOBnDg8KCpzIIv7E5fJTUJ+Yb5nbtkhfTSHUq38SnSE/AgMBAAECggEAVeWnJ4gYNsoXarg/0CQiqjBwQjf67up7k49Z6XBAbpuE4w6uGXNL1dMBWzleRqrMRlD0y2i7pdEadO41ZNhtwTlB1cpluG/HrV6dWTUHXPMxsd9SijI/uUxx7WOywRysi8s4Tc0UjgG/QpdSKaw04n7y5ZGvzGoVTs3iOcw4JKmTZWWh/0mdLBrZ9k9O6HISmU1tnjXhbraZ+CRmsqwW5R5c3FJmYkh6DrrBgaQLpN/99UNwRDDTZarVYxeb1Ywt9pJ4tVDyyZxwAJMwFG8TBVpv3AossbRLf6MXwX4G+PltfNOfsQAIBgjW34Um4rlN5c4mBQofpFFMDX6r7F1mQQKBgQDgCXlkxt6IVQqCpuvyY24XSad/zdcfAphRg3We8jttJuxhfp7vD5N2p0fqes9ExVADtW/qdEwNypci+y778IUVEfQSUlKZR5LuyKs0MSVSEXOVh3kayWz7ViBm0v+PuyfyWtHZT62QF01634U7RXQtaENDpoM1TdUEvDtivBsAUwKBgQDc1p5demnhCJGDOFtJ/HQ/uTbERCvWnLfbCdA+r5ITfvXvZb9ny1ZQNIgAYT7Bz8Wv3koTY/8yuDtD1Tn2NxZO+vZSloTKeCc+TwHrrMEbw5pkXtTAJqwkROcPQmKyikJvbybvuoGsGfDApBgrNTu2C6WEp6nbm2jsYYlo8xPt5QKBgHrVNvWGA6SD28tecabn1Qh3nZp45SVfatvmU6rd7K8f1CJwJfu0colvlgJ9av6tn/0okT/em4REOzE2vr/rU0wGF+ejsx3PHERHSXbIK0ofv1uQd1m0bkgvJHy72tqXJoO+P6gCA4so+5YOzyF8JwJuU9LXjMDA97Ys7fJoZ/V5AoGAQBZ8kO40idzdtbIGNBtydQfAEAcknbyPUp6vc3GqN8QxhQ8HFA0beF02Vr3h44bzfM1KvAPaDJMQh+f6USy9aaBFGcSattNVuCxfh1B0HiWOybAHl9fhPwlL8M64K84Z55LfPtTmStagWpeEeA2TzYnlt/CcR6H7OhY4NV/Nmc0CgYEAw6nKzwHpMQ4R4jI3cb9QfduuRV9C6A0lmDTzJOeg0KCjwbBcDTFYE5JpIrd19b4m8+e2pJXKrNOZmIj7QYKls7ea0USbCxplBSJ/wooUTyQOFnbrF0B3CXaoDCtlZ03CTfL4f19AnR68jFlC0k6lBvcjlAj0uxXlR14BEPAMThk=";
+
+    // 支付宝公钥,查看地址：https://openhome.alipay.com/platform/keyManage.htm 对应APPID下的支付宝公钥。
+    public static String alipay_public_key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjb9u/4FUJh10/QyWbg6UUmpOio2KDRt6gDII9BAewTMeO0ixQaAs/WSx+VoEklAHuQdtyUkkl7mMNtRqNI4MU5Ia+wtC9zhPNnyEOFYHdz1PYVO8XX4ewYeMIa410lCYOyiBL5Hz0Jx/WBb/OXz1lsLXpRwsnlbUfplfEgmvLWgni/kERD28wOPcGNfnziXmPzXn7Y0Xaa1aXPMXoCvZbYwiX0+oA+bK1RksXFKw6RzhdRzmXBIZkTX+KD1oeHW3vcYF6glxJlIOJWw1oXxCi3Nn2rXBTIrh77of0tAhR+A541y/r6daEeEVeHbsbMRFAbSF/YFXShWmvHEhdN2rEQIDAQAB";
+
+    // 服务器异步通知页面路径  需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
+    public static String notify_url = "http://localhost:8080/alipay.trade.page.pay-JAVA-UTF-8/notify_url.jsp";
+
+    // 页面跳转同步通知页面路径 需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
+    public static String return_url = "http://localhost:8080/alipay.trade.page.pay-JAVA-UTF-8/return_url.jsp";
+
+    // 签名方式
+    public static String sign_type = "RSA2";
+
+    // 字符编码格式
+    public static String charset = "utf-8";
+
+    // 支付宝网关
+    public static String gatewayUrl = "https://openapi.alipaydev.com/gateway.do";
+
+    // 支付宝网关
+    public static String log_path = "C:\\";
+
+
+//↑↑↑↑↑↑↑↑↑↑请在这里配置您的基本信息↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+
+    /**
+     * 写日志，方便测试（看网站需求，也可以改成把记录存入数据库）
+     * @param sWord 要写入日志里的文本内容
+     */
+    public static void logResult(String sWord) {
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(log_path + "alipay_log_" + System.currentTimeMillis()+".txt");
+            writer.write(sWord);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
--- src/main/java/com/leshan/ebook/controller/OrderController.java
+++ src/main/java/com/leshan/ebook/controller/OrderController.java
@@ -1,5 +1,9 @@
 package com.leshan.ebook.controller;
 
+import com.alipay.api.AlipayClient;
+import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.request.AlipayTradePagePayRequest;
+import com.leshan.ebook.config.AlipayConfig;
 import com.leshan.ebook.enums.Status;
 import com.leshan.ebook.mybatis.entity.dto.OrderDto;
 import com.leshan.ebook.service.OrderService;
@@ -9,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -33,5 +39,38 @@ public class OrderController {
         responseResult.setData(orderDto);
 
         return responseResult;
+    }
+
+    //处理支付请求的方法
+    @RequestMapping("/pay")
+    public void pay(HttpServletRequest request, HttpServletResponse response) throws Exception{
+        //获得初始化的AlipayClient
+        AlipayClient alipayClient = new DefaultAlipayClient(AlipayConfig.gatewayUrl, AlipayConfig.app_id, AlipayConfig.merchant_private_key, "json", AlipayConfig.charset, AlipayConfig.alipay_public_key, AlipayConfig.sign_type);
+
+        //设置请求参数
+        AlipayTradePagePayRequest alipayRequest = new AlipayTradePagePayRequest();
+        alipayRequest.setReturnUrl(AlipayConfig.return_url);
+        alipayRequest.setNotifyUrl(AlipayConfig.notify_url);
+
+        //商户订单号，商户网站订单系统中唯一订单号，必填
+        String out_trade_no = new String(request.getParameter("WIDout_trade_no").getBytes("ISO-8859-1"),"UTF-8");
+        //付款金额，必填
+        String total_amount = new String(request.getParameter("WIDtotal_amount").getBytes("ISO-8859-1"),"UTF-8");
+        //订单名称，必填
+        String subject = new String(request.getParameter("WIDsubject").getBytes("ISO-8859-1"),"UTF-8");
+        //商品描述，可空
+        String body = new String(request.getParameter("WIDbody").getBytes("ISO-8859-1"),"UTF-8");
+
+        alipayRequest.setBizContent("{\"out_trade_no\":\""+ out_trade_no +"\","
+                + "\"total_amount\":\""+ total_amount +"\","
+                + "\"subject\":\""+ subject +"\","
+                + "\"body\":\""+ body +"\","
+                + "\"product_code\":\"FAST_INSTANT_TRADE_PAY\"}");
+
+        //请求
+        String result = alipayClient.pageExecute(alipayRequest).getBody();
+
+        //输出
+        response.getWriter().println(result);
     }
 }
